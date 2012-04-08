66385a46d398c167234a146bdd84bcd26e31c012
Ali Mihandoost <ali@mihandoost.com>
2012-04-08 19:59:37 +0330

    php include design pattern

---
+++ php include design pattern/inc/menu.php
@@ -0,0 +1,6 @@
+<ul class="nav">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="page1.php">Page 1</a></li>
+  <li><a href="page2.php">Page 2</a></li>
+  <li><a href="page3.php">Page 3</a></li>
+</ul>
