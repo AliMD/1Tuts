cd9691603058dec43e8e7c40b0f4e8e77a3243ef
Ali Mihandoost <ali@mihandoost.com>
2012-04-10 07:00:03 +0430

    fix "more ..." bug for wp lang

--- basic wordpress theme/index.php
+++ basic wordpress theme/index.php
@@ -7,7 +7,7 @@
 	<?php if(have_posts()){ while(have_posts()){ the_post(); ?>
 		<div class="post">
 			<h2><?php the_title(); ?></h2>
-			<p><?php the_content(__("(more ...)")); ?></p>
+			<p><?php the_content(__("(more...)")); ?></p>
 		</div>
 	<?php } }else{
 		_e("Sorry, no posts matched your criteria.");
