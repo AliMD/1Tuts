c9b1e1bb251d510d9eed86f8bcf8db35068d6b75
unknown <IT@b5l1-tr.(none)>
2012-04-10 20:04:50 +0330

    wordpress theme basic

---
+++ basic wordpress theme/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link type="text/css" rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><a href="#"><img src="" alt="Insert Logo Here" name="Insert_logo" width="20%" height="90" id="Insert_logo" style="background: #8090AB; display:block;" /></a> 
+    <!-- end .header --></div>
---
+++ basic wordpress theme/sidebar.php
@@ -0,0 +1,15 @@
+<div class="sidebar1">
+    <ul class="nav">
+      <li><a href="#">Link one</a></li>
+      <li><a href="#">Link two</a></li>
+      <li><a href="#">Link three</a></li>
+      <li><a href="#">Link four</a></li>
+    </ul>
+	<h2><?php _e('Categories'); ?></h2>
+	<ul>
+		<?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+	</ul>
+	<br>
+    <p> The above links demonstrate a basic navigational structure using an unordered list styled with CSS. Use this as a starting point and modify the properties to produce your own unique look. If you require flyout menus, create your own using a Spry menu, a menu widget from Adobe's Exchange or a variety of other javascript or CSS solutions.</p>
+    <p>If you would like the navigation along the top, simply move the ul.nav to the top of the page and recreate the styling.</p>
+    <!-- end .sidebar1 --></div>
