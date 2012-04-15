fda05ca01acce4940c40c7b3a053d644ac1955af
Ali Mihandoost <ali@mihandoost.com>
2012-04-15 19:59:40 +0330

    del junk

--- php include design pattern/inc/menu.php
+++
@@ -1,6 +0,0 @@
-<ul class="nav">
-  <li><a href="index.php">Home</a></li>
-  <li><a href="page1.php">Page 1</a></li>
-  <li><a href="page2.php">Page 2</a></li>
-  <li><a href="page3.php">Page 3</a></li>
-</ul>
--- php include design pattern/js/main.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-
-window.onload=function(){
-	
-}
