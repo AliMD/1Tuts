41b8d0255a4306d740700c0d4edc2977d6ea9559
Ali Mihandoost <ali@mihandoost.com>
2012-04-15 17:03:09 +0330

    add next and back ToDo: beautiful btn by link and css

--- banner basic/banner.js
+++ banner basic/banner.js
@@ -7,16 +7,33 @@ window.onload=function(){
 	
 	var liBtns=document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
+	var picsLen = liBtns.length;
+	
+	var currentSlide = 0;
 	function go2slide(n){
+		if(n>=picsLen) n=0;
+		if(n<0) n=picsLen-1;
+		
 		divScrl.style.left = -n*picWidth + 'px';
+		currentSlide=n;
 	}
 	
-	for(var i=0;i<liBtns.length;i++){
+	function nextSlide(){
+		go2slide(currentSlide+1)
+	}
+	function prvSlide(){
+		go2slide(currentSlide-1)
+	}
+
+	for(var i=0;i<picsLen;i++){
 		(function(j){
 			liBtns.item(j).onclick=function(){
 				go2slide(j);
 			}
 		})(i);
 	}
+	
+	document.getElementById('next').onclick=nextSlide; //test
+	document.getElementById('prv').onclick=prvSlide;
 	
 }
