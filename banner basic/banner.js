f2bf5f6b2f77bf49354261a716a6f20f19b21285
Ali Mihandoost <ali@mihandoost.com>
2012-04-15 16:29:48 +0330

    befor extend

--- banner basic/banner.js
+++ banner basic/banner.js
@@ -1,16 +1,17 @@
 
 window.onload=function(){
 	
-	var bannerWidth = 980;
+	var picWidth = 980;
 	
 	var divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
 	
 	var liBtns=document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
 	for(var i=0;i<liBtns.length;i++){
-		(function(i){
-			liBtns.item(i).onclick=function(){
-				divScrl.style.left= (-i*bannerWidth) + 'px';
+		(function(index){
+			liBtns.item(index).onclick=function(){
+				alert(index);
+				divScrl.style.left = -index*picWidth + 'px';
 			}
 		})(i);
 	}
