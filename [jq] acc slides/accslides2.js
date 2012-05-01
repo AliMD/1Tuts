694de4e2e5af9ed23112f4c32bf73ed233d054ab
Ali mihandoost <ali@mihandoost.com>
2012-05-01 17:01:01 +0330

    page load action

--- [jq] acc slides/accslides2.js
+++ [jq] acc slides/accslides2.js
@@ -2,6 +2,16 @@ $(function(){
 
 	var slides = $('.accslides .train > div');
 
+
+	slides.stop().animate({
+		'width':'50px'
+	},700);
+
+	slides.eq(0).stop().animate({
+		'width':'450px'
+	},700);
+
+
 	slides.click(function(){
 		
 		slides.stop().animate({
