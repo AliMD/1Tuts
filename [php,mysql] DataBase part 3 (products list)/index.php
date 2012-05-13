83fa40ffba8c361156af16e2855af57970be092c
Ali mihandoost <ali@mihandoost.com>
2012-05-13 16:43:10 +0330

    start sample project

---
+++ [php,mysql] DataBase part 3 (products list)/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="reset.css" />
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+
+<div class="container">
+
+</div>
+
+</body>
+</html>
