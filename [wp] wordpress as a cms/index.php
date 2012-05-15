8317a7c42e00eec686e90152ea5191a366ca94a5
Ali mihandoost <ali@mihandoost.com>
2012-05-15 20:17:23 +0330

    Wordpress as a CMS, basic

---
+++ [wp] wordpress as a cms/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>Oontitled Document</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/reset.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/1styles.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.css" />
+</head>
+<body>
+
+<div class="container">
+	<div class="container-top">
+		<div class="toptooltip right">E: i@ali.md | T: 412 254-6563</div>
+		<div class="clear"></div>
+	</div>
+	<div class="container-menu">
+		<div class="logo left w7"><h1>SCOPE.</h1></div>
+		<div class="menu ml7"></div>
+	</div>
+</div>
+
+</body>
+</html>
