a75ba2df2da96e717e0fbdb9921dad97e2544c87
Ali mihandoost <ali@mihandoost.com>
2012-05-17 17:55:12 +0330

    Basic layout menu

--- [wp] wordpress as a cms/index.php
+++ [wp] wordpress as a cms/index.php
@@ -16,9 +16,32 @@
 	</div>
 	<div class="container-menu">
 		<div class="logo left w7"><h1>SCOPE.</h1></div>
-		<div class="menu ml7 last">menu ...</div>
+		<div class="topmenu ml7 last">
+			<ul>
+				<li><a href="#">menu 1</a></li>
+				<li><a href="#">menu 2</a></li>
+				<li><a href="#">menu 3</a></li>
+				<li><a href="#">menu 4</a></li>
+				<li><a href="#">menu 5</a></li>
+				<li><a href="#">menu 4</a></li>
+				<li><a href="#">menu 5</a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
 		<div class="clear"></div>
 	</div>
+	<div class="container-contents">
+		<div class="sidebar w7 right last">bla bla bla</div>	
+		<div class="content mr7">
+			<div class="post">
+				<h2>Article 1</h2>
+				<p>adsfhg sdgh dyrjk ety jfgjn ery ra dfbn sa aqr </p>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="container-widgets"></div>
+	<div class="container-footer"></div>
 </div>
 <div class="backimage"></div>
 </body>
