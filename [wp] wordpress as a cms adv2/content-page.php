41f785faa79195be448ce9d7a1e22b3f2f5798f8
Ali mihandoost <ali@mihandoost.com>
2012-06-17 20:16:28 +0330

    add meta

--- [wp] wordpress as a cms adv2/content-page.php
+++ [wp] wordpress as a cms adv2/content-page.php
@@ -5,9 +5,31 @@
 ?>
 <article class='post'>
 	<h2><?php the_title(); ?></h2>
+	<?php
+		$pics = get_post_meta($post->ID,'pic');
+		if( count($pics)>0 ){
+	?>
+		<div class="pics">
+		<?php
+			
+			foreach($pics as $pic){
+				echo "<img src='$pic' />";
+			}
+		?>
+		</div>
+	<?php } ?>
 	<p>
 		<?php the_content(); ?>
 	</p>
+	<?php 
+		$price = get_post_meta($post->ID,'price',true);
+
+		if( $price!=null ){
+	?>
+		<div class="price">
+			Price : <?php echo $price; ?>
+		</div>
+	<?php } ?>
 </article>
 <?php
 		}
