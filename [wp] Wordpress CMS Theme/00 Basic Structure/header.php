2f6a73097a1cbec94bb216bf3c3278524e9e692b
Ali mihandoost <ali@mihandoost.com>
2012-08-30 19:44:20 +0330

    title

--- [wp] Wordpress CMS Theme/00 Basic Structure/header.php
+++ [wp] Wordpress CMS Theme/00 Basic Structure/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title><?php bloginfo('name'); wp_title(); ?></title>
 <link href="<?php bloginfo('template_url'); ?>\style.css" rel="stylesheet" type="text/css">
 </head>
 
