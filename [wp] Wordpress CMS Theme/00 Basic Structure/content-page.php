7004488edb9cf8d6274275ef8f88031bcdb91ae8
Ali mihandoost <ali@mihandoost.com>
2012-08-30 19:08:44 +0330

    00 basic structure

---
+++ [wp] Wordpress CMS Theme/00 Basic Structure/content-page.php
@@ -0,0 +1,7 @@
+<div class="content">
+	<?php the_post(); ?>
+	<article>
+		<h1><?php the_title(); ?></h1>
+		<p><?php the_content(); ?></p>
+	</article>
+</div>
---
+++ [wp] Wordpress CMS Theme/00 Basic Structure/sidebar-left.php
@@ -0,0 +1,11 @@
+<div class="sidebar1">
+		<ul class="nav">
+			<?php
+				wp_list_pages(array(
+					'title_li' => ''
+				));
+			?>
+		</ul>
+		<p> The above links demonstrate a basic navigational structure using an unordered list styled with CSS. Use this as a starting point and modify the properties to produce your own unique look. If you require flyout menus, create your own using a Spry menu, a menu widget from Adobe's Exchange or a variety of other javascript or CSS solutions.</p>
+		<p>If you would like the navigation along the top, simply move the ul.nav to the top of the page and recreate the styling.</p>
+		<!-- end .sidebar1 --></div>
