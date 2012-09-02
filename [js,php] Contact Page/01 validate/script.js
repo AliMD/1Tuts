cedaf4fc2fc94526062eab5dd6f61b1c6da6f673
Ali Mihandoost <i@ali.md>
2012-09-02 17:56:54 +0330

    basic validate

--- [js,php] Contact Page/01 validate/script.js
+++ [js,php] Contact Page/01 validate/script.js
@@ -1,5 +1,19 @@
+function validateText(str,len){
+	return str.length >= len;
+}
+
 $(function(){
 	$('#contact-form').submit(function(){
-		alert(1);
+		var err = false;
+
+		var name = $('#name')
+		if( validateText(name.val(),3) ){
+			name.removeClass('err').addClass('ok');
+		} else {
+			name.removeClass('ok').addClass('err');
+			err = true
+		}
+
+		return !err;
 	});
 });
