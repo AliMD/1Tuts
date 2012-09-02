be49f11d1ddafb10c5fec18220b20994ab96cd6b
Ali Mihandoost <i@ali.md>
2012-09-02 20:10:09 +0330

    copy files

---
+++ [js,php] Contact Page/04 php Send Mail/script.js
@@ -0,0 +1,48 @@
+function validateText(str,len){
+	return str.length >= len;
+}
+
+function validateEmail(str){
+	return (validateText(str,7) && str.indexOf('@')>0);
+}
+
+$(function(){
+	$('#contact-form').submit(function(){
+		var target, err = false;
+
+		target = $('#name');
+		if( validateText(target.val(),3) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#subject');
+		if( validateText(target.val(),5) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#mail');
+		if( validateEmail(target.val()) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#txt');
+		if( validateText(target.val(),10) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		return !err;
+
+	});
+});
