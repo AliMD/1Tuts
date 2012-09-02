e8e36ad7f98ef0eee875c3e48bfb0e4d50f32171
Ali Mihandoost <i@ali.md>
2012-09-02 18:23:50 +0330

    email validate part 1

--- [js,php] Contact Page/01 validate/script.js
+++ [js,php] Contact Page/01 validate/script.js
@@ -2,11 +2,20 @@ function validateText(str,len){
 	return str.length >= len;
 }
 
+function validateEmail(str){
+	if(str.indexOf('@')>0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+
 $(function(){
 	$('#contact-form').submit(function(){
 		var err = false;
 
-		var name = $('#name')
+		var name = $('#name');
 		if( validateText(name.val(),3) ){
 			name.removeClass('err').addClass('ok');
 		} else {
@@ -14,7 +23,23 @@ $(function(){
 			err = true
 		}
 
-		var subject = $('#subject')
+		var email = $('#mail');
+		if( validateEmail(email.val()) ){
+			email.removeClass('err').addClass('ok');
+		} else {
+			email.removeClass('ok').addClass('err');
+			err = true
+		}
+
+		var subject = $('#subject');
+		if( validateText(subject.val(),5) ){
+			subject.removeClass('err').addClass('ok');
+		} else {
+			subject.removeClass('ok').addClass('err');
+			err = true
+		}
+
+		var subject = $('#subject');
 		if( validateText(subject.val(),5) ){
 			subject.removeClass('err').addClass('ok');
 		} else {
