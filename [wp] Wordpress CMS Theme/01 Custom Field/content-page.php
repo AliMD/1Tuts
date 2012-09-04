c73d6a03288c6779848a31d30b64c3dc582af8ed
Ali mihandoost <ali@mihandoost.com>
2012-09-04 17:55:41 +0330

    add post custom

--- [wp] Wordpress CMS Theme/01 Custom Field/content-page.php
+++ [wp] Wordpress CMS Theme/01 Custom Field/content-page.php
@@ -1,12 +1,27 @@
 <div class="content">
 	<?php
 		if( have_posts() ) {
-	?>
+			the_post();
 
-		<?php the_post(); ?>
+			$meta = get_post_custom();
+	?>
 		<article>
 			<h1><?php the_title(); ?></h1>
 			<p><?php the_content(); ?></p>
+			<?php
+				if( count($meta['price'])>0 ){
+					$price = $meta['price'][0];
+					echo "<p>Price : <b>$price$</b></p>";
+				}
+
+				if(count($meta['img'])>0){
+					echo "<div class='post_images'>";
+					foreach ($meta['img'] as $img) {
+						echo "<img src='$img' width='150' />";
+					}
+					echo "</div>";
+				}
+			?>
 		</article>
 
 	<?php
