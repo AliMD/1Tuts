1886af3fcdde5258c56dfbc521d87836c39d0c83
Ali Mihandoost <i@ali.md>
2012-09-06 17:32:23 +0330

    send mail

--- [js,php] Contact Page/04 php Send Mail/sendmail.php
+++ [js,php] Contact Page/04 php Send Mail/sendmail.php
@@ -2,13 +2,26 @@
 
 error_reporting(E_ALL ^ E_NOTICE);
 
+$admin = 'info@mydomain.com';
+
 $name    = $_POST['name'];
 $email   = $_POST['mail'];
 $subject = $_POST['subject'];
 $text    = $_POST['txt'];
 
-if( strlen($name) > 2 && strlen($email)>6 && strlen($subject)>5 && strlen($text)>10 ){
-	// send
+if( strlen($name)>=3 && strlen($email)>=7 && strlen($subject)>=5 && strlen($text)>=10 ){
+	if( @mail (
+			$admin,
+			"mydomain.com contact : $subject",
+			$text,
+			"From:$name <$email" )
+	){
+		echo '<h2>Mail sent</h2>';
+	}else{
+		echo '<h2>Error in sending mail.</h2>';
+	}
 }else{
 	echo '<h2>Access Restricted !</h2>';
 }
+
+?>
