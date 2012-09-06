350077884b0ad53612805d125f7f8f45f234489b
Ali Mihandoost <i@ali.md>
2012-09-06 17:22:09 +0330

    send mail basic structure

--- [js,php] Contact Page/04 php Send Mail/sendmail.php
+++ [js,php] Contact Page/04 php Send Mail/sendmail.php
@@ -1,3 +1,14 @@
 <?php
 
-// your homework ...
+error_reporting(E_ALL ^ E_NOTICE);
+
+$name    = $_POST['name'];
+$email   = $_POST['mail'];
+$subject = $_POST['subject'];
+$text    = $_POST['txt'];
+
+if( strlen($name) > 2 && strlen($email)>6 && strlen($subject)>5 && strlen($text)>10 ){
+	// send
+}else{
+	echo '<h2>Access Restricted !</h2>';
+}
