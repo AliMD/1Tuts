bb13941226730240bb1b1d32620d50095714567b
Ali Mihandoost <i@ali.md>
2012-09-06 17:34:22 +0330

    add html

--- [js,php] Contact Page/04 php Send Mail/sendmail.php
+++ [js,php] Contact Page/04 php Send Mail/sendmail.php
@@ -1,27 +1,36 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sending mail ...</title>
+</head>
+<body>
+	<?php
+		error_reporting(E_ALL ^ E_NOTICE);
 
-error_reporting(E_ALL ^ E_NOTICE);
+		$admin = 'info@mydomain.com';
 
-$admin = 'info@mydomain.com';
+		$name    = $_POST['name'];
+		$email   = $_POST['mail'];
+		$subject = $_POST['subject'];
+		$text    = $_POST['txt'];
 
-$name    = $_POST['name'];
-$email   = $_POST['mail'];
-$subject = $_POST['subject'];
-$text    = $_POST['txt'];
+		if( strlen($name)>=3 && strlen($email)>=7 && strlen($subject)>=5 && strlen($text)>=10 ){
+			if( @mail (
+					$admin,
+					"mydomain.com contact : $subject",
+					$text,
+					"From:$name <$email" )
+			){
+				echo '<h2>Mail sent</h2>';
+			}else{
+				echo '<h2>Error in sending mail.</h2>';
+			}
+		}else{
+			echo '<h2>Access Restricted !</h2>';
+		}
+	?>
 
-if( strlen($name)>=3 && strlen($email)>=7 && strlen($subject)>=5 && strlen($text)>=10 ){
-	if( @mail (
-			$admin,
-			"mydomain.com contact : $subject",
-			$text,
-			"From:$name <$email" )
-	){
-		echo '<h2>Mail sent</h2>';
-	}else{
-		echo '<h2>Error in sending mail.</h2>';
-	}
-}else{
-	echo '<h2>Access Restricted !</h2>';
-}
-
-?>
+	<a href="./">Back</a>
+</body>
+</html>
