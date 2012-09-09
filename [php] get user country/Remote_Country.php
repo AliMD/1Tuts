446d8d4dc39bac459d675f659bd163ca0a701e6e
Ali mihandoost <ali@mihandoost.com>
2012-09-09 17:26:25 +0330

    update for XX

--- [php] get user country/Remote_Country.php
+++ [php] get user country/Remote_Country.php
@@ -1,6 +1,10 @@
 <?php
+	$api_url = 'http://api.hostip.info/country.php';
 
-$api_url = "http://api.hostip.info/country.php";
-$country = file_get_contents("$api_url?ip=$_SERVER[REMOTE_ADDR]");
+	$remote_ip = $_SERVER['REMOTE_ADDR'];
 
-echo "country : $country";
+	$country = file_get_contents("$api_url?ip=$remote_ip");
+
+	if($country=='XX') $country = 'Local or iran !';
+
+	echo "country : $country";
