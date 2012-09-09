caf12efb69e7258854d7ad111f6b35a6ac07fe5e
Ali mihandoost <ali@mihandoost.com>
2012-09-09 18:09:28 +0330

    image link with thumbnail

--- [wp] Wordpress CMS Theme/02 Custom Post Img/content-page.php
+++ [wp] Wordpress CMS Theme/02 Custom Post Img/content-page.php
@@ -16,8 +16,10 @@
 
 				if(count($meta['img'])>0){
 					echo "<div class='post_images'>";
-					foreach ($meta['img'] as $img) {
-						echo "<img src='$img' width='150' />";
+					foreach ($meta['img'] as $img_id) {
+						$img_small = wp_get_attachment_image($img_id,'thumbnail'); // default: thumbnail
+						$img_larg = wp_get_attachment_image_src($img_id,'large'); // array
+						echo "<a href='$img_larg[0]'>$img_small</a>"; // $img_larg[0] -> image url
 					}
 					echo "</div>";
 				}
