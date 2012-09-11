b35207c52e5e000dd52513ec1a1a5b637b0f7aad
Ali mihandoost <ali@mihandoost.com>
2012-09-11 17:46:17 +0330

    copy 04

---
+++ [wp] Wordpress CMS Theme/04 Widgets/sidebar-left.php
@@ -0,0 +1,19 @@
+<div class="sidebar1">
+	<ul class="nav">
+		<?php
+			wp_list_pages(array(
+				'title_li' => ''
+			));
+		?>
+	</ul>
+<?php
+	global $template;
+	if($template =='products'){
+?>
+
+	<p>dsfg sdfg sdfg sdtwe hsdtgh sdfh sdfdsfg sdfg sdfg sdtwe hsdtgh sdfh sdfdsfg sdfg sdfg sdtwe hsdtgh sdfh sdfdsfg sdfg sdfg sdtwe hsdtgh sdfh sdfdsfg sdfg sdfg sdtwe hsdtgh sdfh sdf</p>
+
+<?php
+	}
+?>
+</div>
