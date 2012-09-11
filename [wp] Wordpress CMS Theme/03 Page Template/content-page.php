5c30ced452b9522b99025b401aef5090e4b576b0
Ali mihandoost <ali@mihandoost.com>
2012-09-11 16:55:09 +0330

    separate 404 page template

--- [wp] Wordpress CMS Theme/03 Page Template/content-page.php
+++ [wp] Wordpress CMS Theme/03 Page Template/content-page.php
@@ -1,39 +1,13 @@
 <div class="content">
-	<?php
-		if( have_posts() ) {
-			the_post();
-
-			$meta = get_post_custom();
-	?>
+<?php
+	if( have_posts() ) {
+		the_post();
+?>
 		<article>
 			<h1><?php the_title(); ?></h1>
 			<p><?php the_content(); ?></p>
-			<?php
-				if( count($meta['price'])>0 ){
-					$price = $meta['price'][0];
-					echo "<p>Price : <b>$price$</b></p>";
-				}
-
-				if(count($meta['img'])>0){
-					echo "<div class='post_images'>";
-					foreach ($meta['img'] as $img_id) {
-						$img_small = wp_get_attachment_image($img_id,'thumbnail'); // default: thumbnail
-						$img_larg = wp_get_attachment_image_src($img_id,'large'); // array
-						echo "<a href='$img_larg[0]'>$img_small</a>"; // $img_larg[0] -> image url
-					}
-					echo "</div>";
-				}
-			?>
 		</article>
-
-	<?php
-		} else {
-	?>
-		<article>
-			<h1>Page not Found</h1>
-			<p>The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.</p>
-		<article>
-	<?php
-		}
-	?>
+<?php
+	}
+?>
 </div>
