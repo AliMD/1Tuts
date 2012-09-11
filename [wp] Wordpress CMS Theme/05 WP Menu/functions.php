ecacdd96527ed8f23d3e85fffdd90d7903563f37
Ali mihandoost <ali@mihandoost.com>
2012-09-11 20:12:21 +0330

    add wp menu

--- [wp] Wordpress CMS Theme/05 WP Menu/functions.php
+++ [wp] Wordpress CMS Theme/05 WP Menu/functions.php
@@ -1,6 +1,8 @@
 <?php
 	add_theme_support('menus');
 
+	register_nav_menu("mainmenu", 'Right Menu');
+
 	register_sidebar(array(
 		'name' => 'Sidebar Right',
 		'id' => 'sidebar-r',
--- [wp] Wordpress CMS Theme/05 WP Menu/sidebar-left.php
+++ [wp] Wordpress CMS Theme/05 WP Menu/sidebar-left.php
@@ -1,8 +1,23 @@
 <div class="sidebar1">
 	<ul class="nav">
 		<?php
-			wp_list_pages(array(
-				'title_li' => ''
+			wp_nav_menu(array(
+				'theme_location'  => 'mainmenu',
+				'menu'            => '', 
+				'container'       => 'div', 
+				'container_class' => 'menu-{menu slug}-container', 
+				'container_id'    => '',
+				'menu_class'      => 'nav', 
+				'menu_id'         => '',
+				'echo'            => true,
+				'fallback_cb'     => 'wp_page_menu',
+				'before'          => '',
+				'after'           => '',
+				'link_before'     => '',
+				'link_after'      => '',
+				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'           => 2,
+				'walker'          => ''
 			));
 		?>
 	</ul>
