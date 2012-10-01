84dc0be6dc0116c1394ebf180a612c9333935cd1
Ali mihandoost <ali@mihandoost.com>
2012-10-01 06:24:14 +0330

    underdev

---
+++ [css3,js] UnderDev/scripts/underdev.js
@@ -0,0 +1,38 @@
+// Zepto/jQuery fadeLoop plugin for fade slide show effects by ali.md
+
+(function($){
+	$.extend($.fn,{
+		fadeLoop :function(options){
+
+			options=$.extend({
+				duration : 2500,
+				delay : 2000,
+				startIndex : -1,
+				fadeFirstImage : true
+			},options);
+
+			var nextPic,
+				pics    =this,
+				indx    =options.startIndex,
+				plen    =this.length,
+				fadeIn  ={opacity:1},
+				fadeOut ={opacity:0},
+				ease = !!window.Zepto ? 'ease-in-out' : 'swing';
+
+			pics.css(fadeOut);
+			options.fadeFirstImage || pics.eq(0).css(fadeIn);
+
+			(nextPic=function(){
+				pics.eq(indx).animate(fadeOut,options.duration,ease);
+				indx=indx<plen-1?indx+1:0;
+				pics.eq(indx).animate(fadeIn,options.duration,ease,function(){
+					setTimeout(nextPic,options.delay);
+				});
+			})();
+		}
+	});
+})($)
+
+$(function(){
+	$('.backimg > div').fadeLoop();
+});
