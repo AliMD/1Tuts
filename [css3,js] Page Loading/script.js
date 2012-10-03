092d5efa6b7917b0a33238a3b5fa24ae1832e591
Ali mihandoost <ali@mihandoost.com>
2012-10-03 09:48:32 +0330

    unload animate

--- [css3,js] Page Loading/script.js
+++ [css3,js] Page Loading/script.js
@@ -1,3 +1,11 @@
 window.onload=function(){
 	$('body').removeClass('preload');
+	$('menu.top a').click(function(){
+		$('body').addClass('unload');
+		var newurl = $(this).attr('href');
+		setTimeout(function(){
+			window.location.href = newurl;
+		},700);
+		return false; // prevent to load page.
+	});
 };
