5a4760adcc2d7a8717e4454c7936af68eae5e145
Ali mihandoost <ali@mihandoost.com>
2012-10-06 08:24:45 +0330

    02 Load with hash

--- [js] Basic Ajax/02 Load with hash/script.js
+++ [js] Basic Ajax/02 Load with hash/script.js
@@ -1,14 +1,33 @@
 $(function(){
+	var loadpage = function(url){
+		url = url.length > 2 ? url.substr(1) : 'index.html';
+		$('.main-content').load(url+' .main-content');
+	}
+
 	$('div.loading').css({
 		opacity:1,
 		display:'block'
 	});
 
+	var pageUrl = window.location.href;
+	sharPos = pageUrl.indexOf('#');
+	if(sharPos){
+		loadpage(pageUrl.substr(sharPos));
+		pageUrl = pageUrl.substr(0,sharPos);
+	}
+
 	$('div.menu a').click(function(){
 		var url = $(this).attr('href');
-		$('.main-content').load(url+' .main-content');
+		window.location.href = pageUrl + '#' + url;
+		//$('.main-content').load(url+' .main-content');
 		return false;
 	});
+
+	window.onhashchange = function(){
+		loadpage(window.location.hash);
+	}
+
+	
 });
 
 window.onload = function(){
