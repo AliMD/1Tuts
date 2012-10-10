9eb62fd65ea92290fa2011d311dc7d3bb7fd6f5e
Ali mihandoost <ali@mihandoost.com>
2012-10-10 10:46:23 +0330

    remove old tuts

--- [wp] wordpress as a cms adv2/content-gallery.php
+++
@@ -1,3 +0,0 @@
-<div class="gallery">
-	gallery ...
-</div>
--- [wp] wordpress as a cms adv2/footer.php
+++
@@ -1,9 +0,0 @@
-	<div class="container-footer">
-		<?php
-			get_template_part('menu', 'footer');
-		?>
-	</div>
-</div>
-<div class="backimage"></div>
-</body>
-</html>
