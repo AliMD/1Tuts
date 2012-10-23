9169d74e2b9098cf0bd4e2099deae58404843907
Ali Mihandoost <i@ali.md>
2012-10-23 12:42:20 +0330

    fix content hovering bug

--- [css3,js] UnderDev/scripts/underdev.js
+++ [css3,js] UnderDev/scripts/underdev.js
@@ -8,7 +8,9 @@
 				duration : 2500,
 				delay : 2000,
 				startIndex : -1,
-				fadeFirstImage : true
+				fadeFirstImage : true,
+				zIndex : -3,
+				zIndexAct : -2
 			},options);
 
 			var nextPic,
@@ -16,17 +18,19 @@
 				indx    =options.startIndex,
 				plen    =this.length,
 				fadeIn  ={opacity:1},
-				fadeOut ={opacity:0},
+				fadeOut ={opacity:0}
 				ease = !!window.Zepto ? 'ease-in-out' : 'swing';
 
-			pics.css(fadeOut);
-			options.fadeFirstImage || pics.eq(0).css(fadeIn);
+			pics.css(fadeOut).css({'z-index':options.zIndex});
+			options.fadeFirstImage || pics.eq(0).css(fadeIn).css({'z-index':options.zIndexAct});
 
 			(nextPic=function(){
-				pics.eq(indx).animate(fadeOut,options.duration,ease);
+				pics.eq(indx).animate(fadeOut,options.duration,ease,function(){
+					 $(this).css({'z-index':options.zIndex});
+				});
 				indx=indx<plen-1?indx+1:0;
-				pics.eq(indx).animate(fadeIn,options.duration,ease,function(){
-					setTimeout(nextPic,options.delay);
+				pics.eq(indx).css({'z-index':options.zIndexAct}).animate(fadeIn,options.duration,ease,function(){
+					setTimeout(nextPic,options.freez);
 				});
 			})();
 		}
@@ -34,8 +38,10 @@
 
 	$(function(){
 		$('.backimg > div').fadeLoop({
-			duration : 1500,
-			delay : 2000
+			duration : 1000, // zaman animation ha
+			freez : 1500, // zaman tavaghof barye har slide
+			zIndex : -3, // z-index hameye slide ha dar halate addi
+			zIndexAct : -2 // z-index slide active ke bayad balatar bashe ta link ha ghabel click shodan bashan
 		});
 	});
 
