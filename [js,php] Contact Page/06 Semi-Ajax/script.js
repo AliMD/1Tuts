553fc49a82d1c8e90d70bc9f51de293ffd130cb3
Ali Mihandoost <i@ali.md>
2012-12-19 15:42:30 +0330

    06 Semi-Ajax send mail

---
+++ [js,php] Contact Page/06 Semi-Ajax/script.js
@@ -0,0 +1,62 @@
+function validateText(str,len){
+	return str.length >= len;
+}
+
+function validateEmail(str){
+	var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+	return emailPattern.test(str);
+}
+
+$(function(){
+	$('#contact-form').submit(function(){
+		var target, err = false;
+
+		target = $('#name');
+		if( validateText(target.val(),3) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#subject');
+		if( validateText(target.val(),5) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#mail');
+		if( validateEmail(target.val()) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		target = $('#txt');
+		if( validateText(target.val(),10) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		}
+
+		if(!err){
+			$('#ifrm').animate({
+				height:'75px'
+			},500);
+		}
+
+		return !err;
+
+	});
+
+	$('#reset').click(function(){
+		$('#ifrm').animate({
+			height:'0px'
+		},200);
+	});
+});
---
+++ [js,php] Contact Page/06 Semi-Ajax/sendmail.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sending mail ...</title>
+	<style type="text/css">
+		body {
+			background-color: #F4F4F4;
+		}
+
+		.ok {
+			color:#23B3AB;
+		}
+
+		.err {
+			color:#BD5B3D;
+		}
+	</style>
+</head>
+<body>
+	<?php
+		error_reporting(E_ALL ^ E_NOTICE);
+
+		$admin = 'info@mydomain.com';
+
+		$name    = $_POST['name'];
+		$email   = $_POST['mail'];
+		$subject = $_POST['subject'];
+		$text    = "Mydomain.com Contact US :\r\n$_POST[txt]";
+
+		if( strlen($name)>=3 && strlen($email)>=7 && strlen($subject)>=5 && strlen($text)>=10 ){
+			if( @mail (
+					$admin,
+					"mydomain.com contact : $subject",
+					$text,
+					"From:contact@thisdomain.com\r\nReply-To:$name <$email>" );
+			){
+				echo '<h2 class="ok">Mail sent</h2>';
+			}else{
+				echo '<h2 class="err">Error in sending mail.</h2>';
+			}
+		}else{
+			echo '<h2 class="err">Access Restricted !</h2>';
+		}
+	?>
+</body>
+</html>
